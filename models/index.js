4088a5418d6e6f4a3442cca546742ad577cf8164
Saurabh Patel <saurabhp@masteroapp.com>
2022-06-22 19:51:55 +0530

    fix: setup connect and test

--- models/index.js
+++ models/index.js
@@ -1,8 +1,5 @@
-const fs = require('fs');
-const path = require('path');
 const { Sequelize } = require('sequelize');
 
-const basename = path.basename(__filename);
 const database = process.env.DB_MYSQL_DATABASE ? process.env.DB_MYSQL_DATABASE : 'mastero';
 const username = process.env.DB_MYSQL_USER ? process.env.DB_MYSQL_USER : 'root';
 const password = process.env.DB_MYSQL_PASSWORD ? process.env.DB_MYSQL_PASSWORD : 'root';
@@ -10,26 +7,17 @@ const options = {
   host: process.env.DB_MYSQL_HOST ? process.env.DB_MYSQL_HOST : 'localhost',
   dialect: process.env.DB_MYSQL_DIALECT ? (process.env.DB_MYSQL_DIALECT) : ('mysql'),
 };
-const db = {};
 
-const sequelize = new Sequelize(database, username, password, options);
+const sequelizeConnection = new Sequelize(database, username, password, options);
 
-fs.readdirSync(__dirname)
-  .filter((file) => {
-    return file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.js';
-  })
-  .forEach((file) => {
-    const model = require(path.join(__dirname, file))(sequelize);
-    db[model.name] = model;
-  });
-
-Object.keys(db).forEach((modelName) => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
+/** Test DB Connection is OK. */
+(async () => {
+  try {
+    await sequelizeConnection.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
   }
-});
+})();
 
-db.sequelize = sequelize;
-
-exports.sequelize;
-module.exports = db;
+module.exports = sequelizeConnection;
