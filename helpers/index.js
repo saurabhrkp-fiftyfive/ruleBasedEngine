7727753d37f77aaab3e249175cb68b9df89d7f55
Saurabh Patel <saurabhp@masteroapp.com>
2022-06-22 23:05:18 +0530

    feat: added isFalsey function

--- helpers/index.js
+++ helpers/index.js
@@ -7,3 +7,5 @@ exports.sendResponse = async (res, status = 200, body = { messsage: 'OK' }) => {
 exports.sendErrorResponse = async (next, status = 500, error = { messsage: 'NOT OK' }) => {
   return next(createError(status, error, { expose: true }));
 };
+
+exports.isFalsey = (value) => value === undefined || value === null || (typeof value === "object" && Object.keys(value).length === 0) || (typeof value === "string" && value.trim().length === 0);
