f0bd23bb3464868e322c6182ef1ac027f0a8f5bc
Saurabh Patel <saurabhp@masteroapp.com>
2022-06-22 23:08:12 +0530

    fix: updated completion event handler

--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,14 @@
-const { sendResponse, sendErrorResponse } = require('../helpers');
+const { sendResponse, sendErrorResponse, isFalsey } = require('../helpers');
 const Event = require('../models/events');
+
+const EVENT_NAME = 'CHALLENGE_COMPLETED';
 
 exports.challengeCompletion = async (req, res, next) => {
   try {
-    const { eventName, eventMessage } = req.body;
-    if (eventName === '' && eventMessage === '') return sendResponse(res, 201, { message: 'Event is empty' });
-    const event = await Event.create({ eventName, eventMessage });
+    const { user_id, timespent, challenge_id, completion, score } = req.body;
+    if (isFalsey(user_id) && isFalsey(challenge_id) && isFalsey(completion)) return sendResponse(res, 201, { message: 'Event is empty' });
+    const user_challenge_attempt = { user_id, timespent, challenge_id, completion, score };
+    const event = await Event.create({ eventName: EVENT_NAME, eventMessage: user_challenge_attempt });
     console.log(`Inserted event: ${event.eventName} with jobId: ${event.jobId}`);
     return sendResponse(res, 200, { message: 'OK', jobId: event.jobId });
   } catch (error) {
