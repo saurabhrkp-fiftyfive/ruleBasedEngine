269dd55c16198641a5914063b815962293b51226
Saurabh Patel <saurabhp@masteroapp.com>
2022-06-23 18:42:20 +0530

    fix: Added model

---
+++ models/demographicKeyLevels.js
@@ -0,0 +1,14 @@
+const { DataTypes } = require('sequelize');
+const mysqlConnection = require('./connectMysql');
+
+const DemographicKeyLevels = mysqlConnection.define('DemographicKeyLevels',
+  {
+    demographicKey: { type: DataTypes.STRING, allowNull: false },
+    levelId: { type: DataTypes.INTEGER, allowNull: false },
+    order: { type: DataTypes.INTEGER, allowNull: false },
+    deleteFlag: { type: DataTypes.BOOLEAN, allowNull: false, defaultValue: false }
+  },
+  { tableName: 'demographic_key_levels' }
+);
+
+module.exports = DemographicKeyLevels;
