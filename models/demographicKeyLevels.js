9e674fade536969d1abd55910be04346f81c388a
Saurabh Patel <saurabhp@masteroapp.com>
2022-06-26 23:46:36 +0530

    fix: Updated demographic_key_levels model

--- models/demographicKeyLevels.js
+++ models/demographicKeyLevels.js
@@ -4,6 +4,7 @@ const mysqlConnection = require('./connectMysql');
 const DemographicKeyLevels = mysqlConnection.define('DemographicKeyLevels',
   {
     demographicKey: { type: DataTypes.STRING, allowNull: false },
+    companyId: { type: DataTypes.INTEGER, allowNull: false },
     levelId: { type: DataTypes.INTEGER, allowNull: false },
     order: { type: DataTypes.INTEGER, allowNull: false },
     deleteFlag: { type: DataTypes.BOOLEAN, allowNull: false, defaultValue: false }
